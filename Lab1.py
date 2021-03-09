473e00272cf82f929de4aeff002a83caa62d5a4f
lu656 <lu656@purdue.edu>
2021-03-09 01:04:10 -0500

    Accuracy minimums met

--- Lab1.py
+++ Lab1.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.utils import to_categorical
 import random
+import matplotlib.pyplot as plt
 
 
 random.seed(1618)
@@ -15,15 +16,15 @@ tf.random.set_seed(1618)
 #tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-ALGORITHM = "guesser"
-#ALGORITHM = "tf_net"
-#ALGORITHM = "tf_conv"
+# ALGORITHM = "guesser"
+# ALGORITHM = "tf_net"
+ALGORITHM = "tf_conv"
 
-DATASET = "mnist_d"
-#DATASET = "mnist_f"
-#DATASET = "cifar_10"
-#DATASET = "cifar_100_f"
-#DATASET = "cifar_100_c"
+# DATASET = "mnist_d"
+DATASET = "mnist_f"
+# DATASET = "cifar_10"
+# DATASET = "cifar_100_f"
+# DATASET = "cifar_100_c"
 
 if DATASET == "mnist_d":
     NUM_CLASSES = 10
@@ -38,11 +39,26 @@ elif DATASET == "mnist_f":
     IZ = 1
     IS = 784
 elif DATASET == "cifar_10":
-    pass                                 # TODO: Add this case.
+    # pass                                 # TODO: Add this case.
+    NUM_CLASSES = 10
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = 784
 elif DATASET == "cifar_100_f":
-    pass                                 # TODO: Add this case.
+    # pass                                 # TODO: Add this case.
+    NUM_CLASSES = 100
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = 784
 elif DATASET == "cifar_100_c":
-    pass                                 # TODO: Add this case.
+    # pass                                 # TODO: Add this case.
+    NUM_CLASSES = 20
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = 784
 
 
 #=========================<Classifier Functions>================================
@@ -57,13 +73,126 @@ def guesserClassifier(xTest):
 
 
 def buildTFNeuralNet(x, y, eps = 6):
-    pass        #TODO: Implement a standard ANN here.
-    return None
+    # pass        #TODO: Implement a standard ANN here.
+    model = None
+    if DATASET == 'cifar_100_f':
+        model = keras.Sequential([
+                    keras.layers.Dense(512, activation='sigmoid'),
+                    keras.layers.Dense(512, activation='sigmoid'),
+                    keras.layers.Dense(100)
+                ])
+    elif DATASET == 'cifar_100_c':
+        model = keras.Sequential([
+                    keras.layers.Dense(512, activation='sigmoid'),
+                    keras.layers.Dense(512, activation='sigmoid'),
+                    keras.layers.Dense(20)
+                ])
+    else:
+        model = keras.Sequential([
+                    keras.layers.Dense(512, activation='sigmoid'),
+                    keras.layers.Dense(512, activation='sigmoid'),
+                    keras.layers.Dense(10)
+                ])
+
+    model.compile(optimizer='adam', 
+                  loss='mean_squared_error',
+                  metrics=['accuracy'])
+    model.fit(x, y, epochs=eps)
+    return model
 
 
 def buildTFConvNet(x, y, eps = 10, dropout = True, dropRate = 0.2):
-    pass        #TODO: Implement a CNN here. dropout option is required.
-    return None
+    # pass        #TODO: Implement a CNN here. dropout option is required.
+    model = tf.keras.Sequential()
+    # print(x, y)
+    if 'cifar' in DATASET:
+        model.add(tf.keras.layers.Conv2D(32, (3, 3), use_bias=False, input_shape=(IH, IW, IZ)))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation('elu'))
+        if dropout:
+            model.add(tf.keras.layers.Dropout(dropRate))
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), use_bias=False))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation('elu'))
+        if dropout:
+            model.add(tf.keras.layers.Dropout(dropRate))
+        model.add(tf.keras.layers.Conv2D(256, (3, 3), use_bias=False))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation('elu'))
+        if dropout:
+            model.add(tf.keras.layers.Dropout(dropRate))
+        model.add(tf.keras.layers.Flatten())
+        if DATASET == 'cifar_100_c':
+            model.add(tf.keras.layers.Dense(1000, activation='elu'))
+            model.add(tf.keras.layers.Dense(20, activation='softmax'))
+        elif DATASET == 'cifar_100_f':
+            model.add(tf.keras.layers.Dense(1000, activation='elu'))
+            model.add(tf.keras.layers.Dense(100, activation='softmax'))
+        else:
+            model.add(tf.keras.layers.Dense(100, activation='elu'))
+            model.add(tf.keras.layers.Dense(10, activation='softmax'))
+        model.compile(optimizer='adam',
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+        model.fit(x, y, epochs=eps)
+    elif DATASET == 'mnist_d':
+        model.add(tf.keras.layers.Conv2D(32, (3, 3), use_bias=False, input_shape=(IH, IW, IZ)))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation('elu'))
+        if dropout:
+            model.add(tf.keras.layers.Dropout(dropRate))
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), use_bias=False))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation('elu'))
+        if dropout:
+            model.add(tf.keras.layers.Dropout(dropRate))
+        model.add(tf.keras.layers.Conv2D(256, (3, 3), use_bias=False))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation('elu'))
+        if dropout:
+            model.add(tf.keras.layers.Dropout(dropRate))
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(1000, activation='elu'))
+        model.add(tf.keras.layers.Dense(10, activation='softmax'))
+        model.compile(optimizer='adam',
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+        model.fit(x, y, epochs=eps)
+    else:
+        model.add(tf.keras.layers.Conv2D(32, (3, 3), use_bias=False, input_shape=(IH, IW, IZ)))
+        model.add(tf.keras.layers.Conv2D(32, (3, 3), use_bias=False))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation('elu'))
+        if dropout:
+            model.add(tf.keras.layers.Dropout(dropRate))
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), use_bias=False))
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), use_bias=False))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation('elu'))
+        if dropout:
+            model.add(tf.keras.layers.Dropout(dropRate))
+        model.add(tf.keras.layers.Conv2D(256, (3, 3), use_bias=False))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation('elu'))
+        if dropout:
+            model.add(tf.keras.layers.Dropout(dropRate))
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(500, activation='elu'))
+        model.add(tf.keras.layers.Dense(10, activation='softmax'))
+        model.compile(optimizer='adam',
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+        model.fit(x, y, epochs=eps + 10)
+    return model
 
 #=========================<Pipeline Functions>==================================
 
@@ -75,11 +204,17 @@ def getRawData():
         mnist = tf.keras.datasets.fashion_mnist
         (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
     elif DATASET == "cifar_10":
-        pass      # TODO: Add this case.
+        # pass      # TODO: Add this case.
+        cifar = tf.keras.datasets.cifar10
+        (xTrain, yTrain), (xTest, yTest) = cifar.load_data()
     elif DATASET == "cifar_100_f":
-        pass      # TODO: Add this case.
+        # pass      # TODO: Add this case.
+        cifar = tf.keras.datasets.cifar100
+        (xTrain, yTrain), (xTest, yTest) = cifar.load_data(label_mode='fine')
     elif DATASET == "cifar_100_c":
-        pass      # TODO: Add this case.
+        # pass      # TODO: Add this case.
+        cifar = tf.keras.datasets.cifar100
+        (xTrain, yTrain), (xTest, yTest) = cifar.load_data(label_mode='coarse')
     else:
         raise ValueError("Dataset not recognized.")
     print("Dataset: %s" % DATASET)
@@ -157,17 +292,38 @@ def evalResults(data, preds):
     print("Classifier algorithm: %s" % ALGORITHM)
     print("Classifier accuracy: %f%%" % (accuracy * 100))
     print()
+    return accuracy * 100
 
 
 
 #=========================<Main>================================================
 
 def main():
+    # nn_type = ['tf_net', 'tf_conv']
+    # dataset_type = ['mnist_d' , 'mnist_f', 'cifar_10', 'cifar_100_f', 'cifar_100_c']
+    # for item in nn_type:
+    #     accuracies = {}
+    #     for t in dataset_type:
+    #         raw = getRawData()
+    #         data = preprocessData(raw)
+    #         model = trainModel(data[0])
+    #         preds = runModel(data[1][0], model)
+    #         acc = evalResults(data[1], preds)
+    #         accuracies[t] = acc
+    #     plt.bar(range(len(accuracies)), list(accuracies.values()), align='center')
+    #     plt.xticks(range(len(accuracies)), list(D.keys()))
+    #     plt.xlabel('Dataset')
+    #     plt.ylabel('Accuracy')
+    #     plt.title(item)
+    #     if item == 'tf_net':
+    #         plt.savefig('./ANN_Accuracy_Plot.pdf')
+    #     else:
+    #         plt.savefig('./CNN_Accuracy_Plot.pdf')
     raw = getRawData()
     data = preprocessData(raw)
     model = trainModel(data[0])
     preds = runModel(data[1][0], model)
-    evalResults(data[1], preds)
+    acc = evalResults(data[1], preds)
 
 
 
